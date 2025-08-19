415a1220f7afc8f56481145726c37afcb488912d
Admin <latuxov@gmail.co>
2025-08-19 23:13:56 +0300

    Handling native ad error

--- AdMobSdk/src/main/java/com/example/admobsdk/NativeAdMobRepository.kt
+++ AdMobSdk/src/main/java/com/example/admobsdk/NativeAdMobRepository.kt
@@ -2,6 +2,7 @@ package com.example.admobsdk
 
 import android.content.Context
 import com.example.adsdkapi.NativeAdEntity
+import com.example.adsdkapi.NativeAdState
 import com.example.adsdkapi.NativeAdsRepository
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
@@ -25,12 +26,13 @@ class NativeAdMobRepository @Inject constructor(
     @param:ApplicationContext private val context: Context
 ) : NativeAdsRepository {
 
+    var failed = 0
     val preparedAds = LinkedBlockingQueue<NativeAd>()
     val adLoader = AdLoader.Builder(context, NATIVE_AD_UNIT_ID).forNativeAd { nativeAd ->
         preparedAds.put(nativeAd)
-
     }.withAdListener(object : AdListener() {
         override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+            failed++
         }
     }).withNativeAdOptions(NativeAdOptions.Builder().build()).build()
 
@@ -40,19 +42,27 @@ class NativeAdMobRepository @Inject constructor(
         }
     }
 
-    //load more ads than needed for a single display in order to be able to show them as quickly as possible.
-    override fun provideTwoAds(): Flow<Pair<NativeAdEntity, NativeAdEntity>> =
+    private fun failedToLoad() = failed >= MAX_ADS_COUNT
+
+    //load more ads than needed for a single display in order to be able to show them as quickly as possible next time
+    override fun provideTwoAds(): Flow<NativeAdState> =
         flow {
+            failed = 0
             var isEmitted = false
             do {
                 preloadAds()
                 if (preparedAds.size >= NEEDED_ADS_AMOUNT) {
                     emit(
-                        Pair(
-                            NativeAdEntity(preparedAds.take()),
-                            NativeAdEntity(preparedAds.take())
+                        NativeAdState.NativeAdsLoaded(
+                            Pair(
+                                NativeAdEntity(preparedAds.take()),
+                                NativeAdEntity(preparedAds.take())
+                            )
                         )
                     )
+                    isEmitted = true
+                } else if (failedToLoad()) {
+                    emit(NativeAdState.Failed)
                     isEmitted = true
                 }
                 delay(PRELOAD_CYCLE_BREAK_IN_MS)
--- domain/src/main/java/com/example/domain/movies/SearchMoviesUseCase.kt
+++ domain/src/main/java/com/example/domain/movies/SearchMoviesUseCase.kt
@@ -1,11 +1,13 @@
 package com.example.domain.movies
 
+import com.example.adsdkapi.NativeAdState
 import com.example.adsdkapi.NativeAdsRepository
 import com.example.domain.DomainListItem
 import com.example.domain.ResponseResultDomain
 import com.example.domain.toDomainListItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
@@ -30,16 +32,28 @@ class SearchMoviesUseCase @Inject constructor(
 
                 is ResponseResult.Error -> flowOf(ResponseResultDomain.Error(responseResult.message))
             }
-        }
+        }.catch { emit(ResponseResultDomain.Error(it.message)) }
     }
 
     private fun handleSuccessResult(movies: List<MovieDomainEntity>): Flow<ResponseResultDomain> {
         return if (movies.size > ITEM_COUNTS_TO_TRIGGER_AD) {
-            nativeAdsRepository.provideTwoAds().map { pairAds ->
+            nativeAdsRepository.provideTwoAds().map { nativeAdState ->
                 movies.map { it.toDomainListItem() as DomainListItem }
                     .toMutableList().apply {
-                        add(AD_INDEX1, DomainListItem.NativeAdListItem(pairAds.first))
-                        add(AD_INDEX3, DomainListItem.NativeAdListItem(pairAds.second))
+                        when (nativeAdState) {
+                            is NativeAdState.NativeAdsLoaded -> {
+                                add(
+                                    AD_INDEX1,
+                                    DomainListItem.NativeAdListItem(nativeAdState.loadedAds.first)
+                                )
+                                add(
+                                    AD_INDEX3,
+                                    DomainListItem.NativeAdListItem(nativeAdState.loadedAds.second)
+                                )
+                            }
+
+                            else -> {}
+                        }
                     }.let {
                         ResponseResultDomain.Success(it)
                     }
