ecce100cbd406374415f6f5100b70397af49fe06
Admin <latuxov@gmail.co>
2025-08-20 23:01:16 +0300

    UI fix for app messages

--- uicomponents/src/main/java/com/example/uicomponents/Message.kt
+++ uicomponents/src/main/java/com/example/uicomponents/Message.kt
@@ -2,17 +2,23 @@ package com.example.uicomponents
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Message(message: String) {
     Box(modifier = Modifier.fillMaxSize()) {
         Text(
             text = message,
-            modifier = Modifier.align(Alignment.Center)
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(12.dp),
+            textAlign = TextAlign.Center
         )
     }
 }
